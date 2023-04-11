9a6522637f0f59c13ba54e7ab3a01d9cc499e30a
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2023-04-11 16:23:30 -0500

    First commit for input and output language codes and verification

--- main.py
+++ main.py
@@ -1,38 +1,33 @@
 """
-This is a hello world add-on for DocumentCloud.
-
-It demonstrates how to write a add-on which can be activated from the
-DocumentCloud add-on system and run using Github Actions.  It receives data
-from DocumentCloud via the request dispatch and writes data back to
-DocumentCloud using the standard API
+DocumentCloud Add-On that translates documents using Google Translate services.
 """
 
 from documentcloud.addon import AddOn
 
 
-class HelloWorld(AddOn):
-    """An example Add-On for DocumentCloud."""
+class Translate(AddOn):
+    """DocumentCloud premium Add-On that translates documents"""
 
     def main(self):
-        """The main add-on functionality goes here."""
-        # fetch your add-on specific data
-        name = self.data.get("name", "world")
-
-        self.set_message("Hello World start!")
-
-        # add a hello note to the first page of each selected document
+        input_lang = self.data.get("input_lang")
+        output_lang = self.data.get("output_lang" 
+        if len(input_lang) != 2:
+            self.set_message(
+                "You submitted an improper ISO language code as an input. "
+                "It is supposed to be two characters in length."
+            )
+            sys.exit(1)
+        if len(output_lang) != 2:
+            self.set_message(
+                "You submitted an improper ISO language code. "
+                "It is supposed to be two characters in length."
+            )
+            sys.exit(1)                         
+                                    
+                                    
         for document in self.get_documents():
-            # get_documents will iterate through all documents efficiently,
-            # either selected or by query, dependeing on which is passed in
-            document.annotations.create(f"Hello {name}!", 0)
-
-        with open("hello.txt", "w+") as file_:
-            file_.write("Hello world!")
-            self.upload_file(file_)
-
-        self.set_message("Hello World end!")
-        self.send_mail("Hello World!", "We finished!")
+            
 
 
 if __name__ == "__main__":
-    HelloWorld().main()
+    Translate().main()
