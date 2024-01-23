f0bb819de2f07cf85edd1e0077b576761a8c9668
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2024-01-23 11:25:16 -0600

    Fix validate(), run pylint and black

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ import sys
 from tempfile import NamedTemporaryFile
 
 from documentcloud.addon import AddOn
+from documentcloud.exceptions import APIError
+
+# pylint: disable=no-name-in-module
 from google.cloud import translate_v2 as translate
 
 import docx
@@ -23,26 +26,31 @@ class Translate(AddOn):
                 "select them and run again."
             )
             return False
-        elif not self.org_id:
+        if not self.org_id:
             self.set_message("No organization to charge.")
+            sys.exit(0)
+        num_chars = 0
+        for document in self.get_documents():
+            num_chars += len(document.full_text)
+        cost = math.ceil(num_chars / 75)
+        try:
+            self.charge_credits(cost)
+        except ValueError:
             return False
-        else:
-            num_chars = 0
-            for document in self.get_documents():
-                num_chars += len(document.full_text)
-            cost = math.ceil(num_chars/75)
-            try:
-                self.charge_credits(cost)
-            except ValueError:
-                return False
+        except APIError:
+            return False
         return True
 
     def dry_run(self, documents):
+        """ Calculates and displays cost to user, no real translation"""
         num_chars = 0
         for doc in documents:
             num_chars += len(doc.full_text)
-        cost = math.ceil(num_chars/75)
-        self.set_message(f"There are {num_chars} characters in this document set. It would cost {cost} AI credits to translate this document set.")
+        cost = math.ceil(num_chars / 75)
+        self.set_message(
+            f"There are {num_chars} characters in this document set. "
+            f"It would cost {cost} AI credits to translate this document set."
+        )
         sys.exit(0)
 
     def setup_credential_file(self):
@@ -50,12 +58,13 @@ class Translate(AddOn):
         credentials = os.environ["TOKEN"]
         # put the contents into a named temp file
         # and set the var to the name of the file
-        gac = NamedTemporaryFile(delete=False)
+        gac = NamedTemporaryFile(delete=False) # pylint: disable=consider-using-with
         gac.write(credentials.encode("ascii"))
         gac.close()
         os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = gac.name
 
     def translate_text(self, text, target_lan, source_lan):
+        """Calls the translate client to do the translation"""
         translate_client = translate.Client()
         result = translate_client.translate(
             text, target_language=target_lan, source_language=source_lan
@@ -63,13 +72,14 @@ class Translate(AddOn):
         return result["translatedText"]
 
     def main(self):
-        # If dry_run is selected, it will calculate the cost of translation. 
+        """ Grabs input and output languages, translates each document """
         if self.data.get("dry_run"):
+            # If dry_run is selected, it will calculate the cost of translation.
             self.dry_run(self.get_documents())
-        
+
         if not self.validate():
-            # if not validated, return immediately
-            return
+            self.set_message("You do not have sufficient AI credits to run this Add-On")
+            sys.exit(0)
 
         # Sets up Google Cloud API Credential file
         self.setup_credential_file()
@@ -82,31 +92,33 @@ class Translate(AddOn):
         # For each document, translate the text and create a text file with the translation
         for document in self.get_documents():
             self.set_message(f"Translating {document.title}...")
-            doc = docx.Document()
-            for page in range(1,document.page_count+1):
+            doc = docx.Document() # pylint: disable=no-member
+            for page in range(1, document.page_count + 1):
                 doc.add_heading(f"Page # {page} Translation", 3)
                 translated_text = str(
-                    self.translate_text(document.get_page_text(page), target_lang, source_lang)
+                    self.translate_text(
+                        document.get_page_text(page), target_lang, source_lang
+                    )
                 )
                 doc.add_paragraph(translated_text)
                 doc.add_page_break()
             doc.save(f"{document.title}-translation_{target_lang}.docx")
-            self.set_message(f"Uploading translation...")
-            # If project ID is specified, upload to that project. Else just upload to user's documents. 
+            self.set_message("Uploading translation...")
+            # If project ID is specified, upload to that project.
             if self.data.get("project_id") is not None:
                 self.client.documents.upload(
                     f"{document.title}-translation_{target_lang}.docx",
                     original_extension="docx",
-                    title=f"{document.title}-translation_{target_lang}", 
-                    access=self.data["access_level"], 
-                    project=self.data.get("project_id")
+                    title=f"{document.title}-translation_{target_lang}",
+                    access=self.data["access_level"],
+                    project=self.data.get("project_id"),
                 )
             else:
                 self.client.documents.upload(
                     f"{document.title}-translation_{target_lang}.docx",
                     original_extension="docx",
-                    title=f"{document.title}-translation_{target_lang}", 
-                    access=self.data["access_level"]
+                    title=f"{document.title}-translation_{target_lang}",
+                    access=self.data["access_level"],
                 )
 
 
